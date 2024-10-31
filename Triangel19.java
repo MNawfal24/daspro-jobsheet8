461ddc38f7bd350b97454a09d59db4a9537fbb0e
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-10-31 08:30:19 +0700

    pembenaran untuk program Triangel

--- Triangel19.java
+++ Triangel19.java
@@ -5,17 +5,15 @@ public class Triangel19 {
 
             System.out.print("Masukkan nilai N= ");
             int N = sc.nextInt();
-            int i = 1;
+            int i = 0;
             while (i <= N) {
-            int j = 0;
+                int j = 0;
                 while (j < i) {
                 System.out.print("*");
-                System.out.print(" ");
                 j++;
             }
+            System.out.println();
+            i++;
         }
-        System.out.println();
-        i++;
-        sc.close();
     }
 }
